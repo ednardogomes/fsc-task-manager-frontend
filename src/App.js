f457e3a91a8f523765dedaf910600ec0df25efde
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-05-21 14:21:01 -0300

    refactor: convert component to Class

--- src/App.js
+++ src/App.js
@@ -3,28 +3,70 @@ import { useState } from "react";
 import "./App.css";
 import TaskItem from "./components/TaskItem";
 
-const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: "1",
-      description: "Estudar Programação",
-      isCompleted: false,
-    },
+// const App = () => {
+//   const [tasks, setTasks] = useState([
+//     {
+//       id: "1",
+//       description: "Estudar Programação",
+//       isCompleted: false,
+//     },
 
-    {
-      id: "2",
-      description: "Ler",
-      isCompleted: true,
-    },
-  ]);
+//     {
+//       id: "2",
+//       description: "Ler",
+//       isCompleted: true,
+//     },
+//   ]);
 
-  return (
-    <>
-      {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} />
-      ))}
-    </>
-  );
-};
+//   return (
+//     <>
+//       {tasks.map((task) => (
+//         <TaskItem key={task.id} task={task} />
+//       ))}
+//     </>
+//   );
+// };
+
+// AGORA VOU MOSTRAR ABAIXO UMA FORMA MAIS ANTIGA QUE ESTÁ PARANDO DE SER USADA
+
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleStateChange = this.handleStateChange.bind(this);
+    this.state = {
+      tasks: [
+        {
+          id: "1",
+          description: "Estudar Programação",
+          isCompleted: false,
+        },
+
+        {
+          id: "2",
+          description: "Ler",
+          isCompleted: true,
+        },
+      ],
+    };
+  }
+
+  handleStateChange() {
+    this.setState({
+      tasks: [],
+    });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.tasks.map((task) => (
+          <TaskItem key={task.id} task={task} />
+        ))}
+        <button onClick={this.handleStateChange}>Limpar Tarefas</button>
+      </>
+    );
+  }
+}
 
 export default App;
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,10 +1,25 @@
-const TaskItem = ({ task }) => {
-  return (
-    <>
-      <h1>{task.description}</h1>
-      <p>{task.isCompleted ? "completa" : "Não completa"} </p>
-    </>
-  );
-};
+// const TaskItem = ({ task }) => {
+//   return (
+//     <>
+//       <h1>{task.description}</h1>
+//       <p>{task.isCompleted ? "completa" : "Não completa"} </p>
+//     </>
+//   );
+// };
+
+// AGORA VOU MOSTRAR ABAIXO UMA FORMA MAIS ANTIGA QUE ESTÁ PARANDO DE SER USADA
+import React from "react";
+class TaskItem extends React.Component {
+  render() {
+    const { task } = this.props;
+
+    return (
+      <>
+        <h1>{task.description}</h1>
+        <p>{task.isCompleted ? "Completa" : "Não completa"}</p>
+      </>
+    );
+  }
+}
 
 export default TaskItem;
