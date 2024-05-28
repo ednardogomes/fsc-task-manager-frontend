4aca034be022406c3c46fe8466d4e72d96b4fbcc
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-05-28 15:25:57 -0300

    feat: use TaskItem component to render tasks

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,5 +1,7 @@
 // import { useEffect } from "react";
 
+
+
 const TaskItem = ({ task }) => {
   // useEffect(() => {
   //   console.log("component was mounted!");
@@ -12,7 +14,19 @@ const TaskItem = ({ task }) => {
     <>
       <div className="task-item-container">
         <div className="task-description">
-          <label className=""></label>
+          <label
+            className={
+              task.isCompleted
+                ? "checkbox-container-completed"
+                : "checkbox-container"
+            }
+          >
+            {task.description}
+            <input type="checkbox" defaultChecked={task.isCompleted} />
+            <span
+              className={task.isCompleted ? "checkmark completed" : "checkmark"}
+            ></span>
+          </label>
         </div>
       </div>
     </>
